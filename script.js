2e7e9af28c72fc82f5751714fc2545832308d60c
Haikoo1 <134221355+Haikoo1@users.noreply.github.com>
2025-02-21 22:00:32 -0400

    - Adicionando arquivos necessários

---
+++ script.js
@@ -0,0 +1,40 @@
+const searchInput = document.getElementById('search-input');
+const resultArtist = document.getElementById("result-artist");
+const resultPlaylist = document.getElementById('result-playlists');
+
+function requestApi(searchTerm) {
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    fetch(url)
+        .then((response) => response.json())
+        .then((result) => displayResults(result))
+}
+
+function displayResults(result) {
+    resultPlaylist.classList.add("hidden")
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
+
+    result.forEach(element => {
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+    });
+
+    resultArtist.classList.remove('hidden');
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle('light');
+    const isLightTheme = document.body.classList.contains('light');
+    themeButton.innerHTML = isLightTheme ? '<span class="fa-light fas fa-moon"></span>' : '<span class="fa-light fas fa-sun"></span>';
+});
+
+document.addEventListener('input', function () {
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm === '') {
+        resultPlaylist.classList.add('hidden');
+        resultArtist.classList.remove('hidden');
+        return
+    }
+    
+    requestApi(searchTerm);
+})
